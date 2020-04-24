ffcd7911a4d512ac822601f56cf262e960f53ab9
Scott Smith <scthsm@outlook.com>
2020-04-24 09:27:22 -0600

    a

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secrets = require('../api/secrets.js')
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+
+module.exports = (req,res,next) => {
+    const token = req.headers.authorization;
+    var grab ;
+
+
+    jwt.verify(token, secrets.jwtSecret, (error,decodedToken) => {
+        if (!error) {
+            req.decodedToken = decodedToken;
+            grab = decodedToken;
+            next();
+          } else {
+            res.status(403).json({ error: "You must be logged in to do that." });
+          }
+    })
+    console.log(grab.username)
+  };
