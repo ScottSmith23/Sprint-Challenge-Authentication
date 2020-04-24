9b31d999d67d9c3dd70c9897753b93ddcdb23e8d
Scott Smith <scthsm@outlook.com>
2020-04-24 10:27:15 -0600

    created react app

--- api/server.spec.js
+++ api/server.spec.js
@@ -83,23 +83,38 @@ describe("server", function () {
       });
 
       describe("GET/jokes", function () { 
-        
-        it("should return 200 OK", function () {
-          return request(server)
-          .get("/")
-          .then(res => {
-              console.log(res.body)
-          expect(res.status).toBe(200);
-              });
-          });
-  
-          it("should return api:up", function () {
+
+        beforeEach(async () => {
+            await db("users").truncate();
+        });
+    
+          it("should return error if no token sent", function () {
               return request(server)
-              .get("/")
+              .get("/api/jokes")
               .then(res => {
-              expect(res.body).toMatchObject({ api: 'up' });
+              expect(res.body).toMatchObject({ error: 'You must be logged in to do that.' });
                   });
               });
+
+              it("should return jokes if token is sent", async function () {
+                var testToken;
+                await request(server)
+                .post("/api/auth/register")
+                .send({username:"nacho",password:"cheese"})
+                await request(server)
+                .post("/api/auth/login")
+                .send({username:"nacho",password:"cheese"})
+                .then(res => {
+                    console.log(res.body)
+                testToken = res.body.token
+                });
+                await request(server)
+                .get("/api/jokes")
+                .set('Authorization', testToken)
+                .then(res => {
+                expect(res.body[0].joke).toBeTruthy();
+                    });
+                });
       });
 
  });
